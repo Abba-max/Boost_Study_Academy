f2157579ab5286c83e9ed52518773076508a93e9
Abba-Max <adjiabba10@gmail.com>
2025-11-26 00:57:30 +0100

    Feature: Login and Registration pages creation

--- BoostAPP/views.py
+++ BoostAPP/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def index(request):
     # This renders the file located at templates/home.html
     return render(request, 'index.html', {})
+
+def login_view(request):
+    return render(request, 'login.html', {})
+
+def registration_view(request):
+    return render(request, 'registration.html', {})
