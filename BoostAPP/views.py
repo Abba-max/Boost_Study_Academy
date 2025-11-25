ed8ae4e24c565ca3911b868f49a7f00e36d7a06c
Abba-Max <adjiabba10@gmail.com>
2025-11-26 00:27:47 +0100

    Template files creation, index view setup and url config

--- BoostAPP/views.py
+++ BoostAPP/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    # This renders the file located at templates/home.html
+    return render(request, 'index.html', {})
